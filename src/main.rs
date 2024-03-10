369c792064db813b0f6f49a269d799a0d5291259
Joe Ruello <joeruello@gmail.com>
2024-03-10 22:17:02 +1100

    part 3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Read, Write},
+    net::TcpListener,
+};
+
+use anyhow::Context;
+use itertools::Itertools;
 
 fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -6,7 +12,22 @@ fn main() -> anyhow::Result<()> {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+                let mut buf = Vec::new();
+                let mut reader = BufReader::new(&mut stream);
+                reader
+                    .read_until(b'\r', &mut buf)
+                    .context("Reading stream")?;
+
+                let (_method, path, _proto) = buf
+                    .splitn(3, |b| b.is_ascii_whitespace())
+                    .collect_tuple()
+                    .context("Should have 3 parts")?;
+
+                match path {
+                    [b'/'] => stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?,
+                    _ => stream.write(b"HTTP/1.1 404 OK\r\n\r\n")?,
+                };
+                println!("received a connection");
             }
             Err(e) => {
                 println!("error: {}", e);
