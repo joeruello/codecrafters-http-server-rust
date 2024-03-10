a403674a1f12bc0e9af77766914e6782721ac52c
Joe Ruello <joeruello@gmail.com>
2024-03-11 00:12:26 +1100

    part 7

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,26 @@ use crate::{
     response::{Body, Response, Status},
 };
 use anyhow::Context;
+use clap::Parser;
 use itertools::Itertools;
-use std::net::{TcpListener, TcpStream};
+use std::{
+    fs::{self, File},
+    net::{TcpListener, TcpStream},
+    path::PathBuf,
+};
 
 mod request;
 mod response;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "/tmp")]
+    directory: PathBuf,
+}
+
 fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
@@ -27,6 +38,7 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn handle_request(mut stream: TcpStream) -> anyhow::Result<()> {
+    let args = Args::parse();
     let req = Request::read(&mut stream).context("Parsing request")?;
 
     let path_parts = req.path.split("/").collect_vec();
@@ -46,6 +58,13 @@ fn handle_request(mut stream: TcpStream) -> anyhow::Result<()> {
                     .as_bytes(),
             );
             Response::from_status_and_body(Status::Ok, body)
+        }
+        Some(&"files") => {
+            let filename = path_parts.get(2).context("Should provide a filename")?;
+            let dir: PathBuf = args.directory;
+            let path = dir.join(filename);
+            let file = fs::read(path)?;
+            Response::from_status_and_body(Status::Ok, Body::new("text/plain", &file))
         }
         Some(&"") => Response::with_status(Status::Ok),
         _ => Response::with_status(Status::NotFound),
